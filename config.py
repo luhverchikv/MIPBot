446cc3befc53b0a453854c06a3108d0a911759c1
luhverchikv <luhverchikv@icloud.com>
2025-10-16 21:29:47 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,10 @@
 from dataclasses import dataclass
 from environs import Env
-from typing import List
 
 
 @dataclass
 class TgBot:
     token: str
-    admin_ids: List[int]
     owner_id: int
 
 
@@ -24,7 +22,6 @@ env.read_env()  # Читаем из .env файла в корне проекта
 config = Config(
     bot=TgBot(
         token=env('BOT_TOKEN'),
-        admin_ids=list(map(int, env.list('ADMIN_IDS'))),
         owner_id=env.int('OWNER_ID')
     ),
 )
